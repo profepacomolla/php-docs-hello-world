22260cbd3cbbd3ddf380839c0379629530d3ee8a
profepacomolla <raul.iticbcn@gmail.com>
2026-05-19 18:08:56 +0200

    Update datos.php
    v5

--- datos.php
+++ datos.php
@@ -1,25 +1,7 @@
 <?php
 
-$host = "msql-cefire01.mysql.database.azure.com";
-$dbname = "prueba";
-$user = "prueba";
-$password = "ContraseñaSegura!";
-
-try {
-
-    $dsn = "mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4";
-
-    $options = [
-        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ];
-
-    $pdo = new PDO($dsn, $user, $password, $options);
-
-    echo "CONEXIÓN OK";
-
-} catch (PDOException $e) {
-
-    echo "ERROR: " . $e->getMessage();
-}
+echo getenv("DB_HOST") . "<br>";
+echo getenv("DB_NAME") . "<br>";
+echo getenv("DB_USER") . "<br>";
+echo getenv("DB_PORT") . "<br>";
 ?>
